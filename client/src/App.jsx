dd35db42c140b06d1d498e7b5e7ca8ab51001b45
Anoop Kumar Burnwal <anoopkumar19062006@gmail.com>
2025-06-23 17:57:37 +0530

    Added Login page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,21 @@
-import React from 'react'
+import React , {lazy} from 'react'
+import {BrowserRouter , Routes , Route} from 'react-router-dom'
+
+const Home = lazy( () => import("./pages/Home") ) // Lazy loading Home component
+const Login = lazy( () => import("./pages/Login") ) // Lazy loading 
+const Chat = lazy( () => import("./pages/Chat") ) // Lazy loading 
+const Groups = lazy( () => import("./pages/Groups") ) // Lazy loading 
 
 function App() {
   return (
-    <div>
-      App
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/chat/:chatId" element={<Chat />} />
+        <Route path="/groups" element={<Groups />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
