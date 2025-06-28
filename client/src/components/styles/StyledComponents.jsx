ba962f0d1f4e80608312805162fc1b02cf64c1aa
Anoop Kumar Burnwal <anoopkumar19062006@gmail.com>
2025-06-28 18:10:50 +0530

    Added home section for chats

--- client/src/components/styles/StyledComponents.jsx
+++ client/src/components/styles/StyledComponents.jsx
@@ -1,5 +1,6 @@
 import {styled} from '@mui/material'
 import {Link as LinkComponent} from 'react-router-dom';
+import { grayColor } from '../../constants/color';
 
 export const VisuallyHiddenInput = styled("input")(
     {
@@ -23,5 +24,21 @@ export const Link = styled(LinkComponent)`
     padding: 1rem;
     &:hover {
         background-color: rgba(0, 0, 0, 0.1);
+    }
+`
+
+export const InputBox = styled("input")`
+    width: 100%;
+    height: 100%;
+    padding: 0 3rem;
+    border-radius: 1.5rem;
+    border: none;
+    outline: none;
+    box-sizing: border-box;
+    background-color: ${grayColor};
+    color: black;
+    
+    &:focus {
+        background-color: rgba(0, 0, 0, 0.2);
     }
 `
--- client/src/pages/Chat.jsx
+++ client/src/pages/Chat.jsx
@@ -1,11 +1,70 @@
-import React from 'react'
+import React, { Fragment, useRef } from 'react'
 import AppLayout from '../components/Layout/AppLayout'
+import { grayColor, orange } from '../constants/color';
+import {IconButton, Stack} from '@mui/material'
+import { AttachFile as AttachFileIcon, Send as SendIcon} from '@mui/icons-material';
+import { InputBox } from '../components/styles/StyledComponents';
 
 const Chat = () => {
+
+  const containerRef = useRef(null);
+
   return (
-    <div>
-      Chats
-    </div>
+    <Fragment>
+      <Stack ref={containerRef}
+        boxSizing={"border-box"}
+        padding={"1rem"}
+        spacing={"1rem"}
+        bgcolor={grayColor}
+        height={"90%"}
+        sx={{
+          overflowX: "hidden",
+          overflowY: "auto",
+        }}
+      >
+        
+        {/* Messages Render */}
+
+      </Stack>
+
+      <form style={{
+        height: "10%"
+      }}>
+
+        <Stack direction={"row"} height={"100%"} padding={"0.5rem"} alignItems={"center"}
+         position={"relative"}>
+
+          <IconButton 
+            sx={{
+              position: "absolute",
+              left: "0.5rem",
+              rotate: "25deg",
+            }}
+          >
+            <AttachFileIcon />
+          </IconButton>
+
+          <InputBox placeholder='Type Message here' />
+
+          <IconButton 
+            type="submit" 
+            sx = {{
+              rotate: "-25deg",
+              backgroundColor: orange,
+              color: "white",
+              marginLeft: "1rem",
+              padding: "0.5rem",
+              "&:hover" :{
+                backgroundColor: "error.dark"
+              }
+          }}>
+            <SendIcon />
+          </IconButton>
+        </Stack>
+
+      </form>
+      
+    </Fragment>
   )
 }
 
