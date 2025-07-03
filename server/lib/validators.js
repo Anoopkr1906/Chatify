43a16bd7bbc0054d06c6ad0df212ae089695f814
Anoop Kumar Burnwal <anoopkumar19062006@gmail.com>
2025-07-03 17:04:29 +0530

    Added validators as middlewares for diff routes

---
+++ server/lib/validators.js
@@ -0,0 +1,71 @@
+import { body, check, param, query, validationResult } from "express-validator";
+import { ErrorHandler } from "../utils/utility.js";
+
+const registerValidator = () => [
+    body("name" , "Please enter a valid name").notEmpty(),
+    body("username" , "Please enter a valid username").notEmpty(),
+    body("password" , "Please enter a valid password").notEmpty(),
+    body("bio" , "Please enter a valid bio").notEmpty(),
+    check("avatar" , "Please upload a valid avatar").notEmpty()
+];
+
+const loginValidator = () => [
+    body("username" , "Please enter a valid username").notEmpty(),
+    body("password" , "Please enter a valid password").notEmpty(),
+];
+
+const newGroupValidator = () => [
+    body("name" , "Please enter a valid Name").notEmpty(),
+    body("members").notEmpty().withMessage("Please enter members").isArray({min: 2 , max:100}).withMessage("Please enter at least 2 members and maximum 100 members"),
+];
+
+const addMemberValidator = () => [
+    body("chatId" , "Please enter a valid chat ID").notEmpty(),
+    body("members").notEmpty().withMessage("Please enter members").isArray({min: 1 , max:97}).withMessage("Members must be an array with at least 1 member and maximum 97 members"),
+];
+
+const removeMemberValidator = () => [
+    body("chatId" , "Please enter a valid chat ID").notEmpty(),
+    body("userId" , "Please enter a valid user ID").notEmpty(),
+];
+
+const sendAttachmentsValidator = () => [
+    body("chatId" , "Please enter a valid chat Id").notEmpty(),
+    check("files").notEmpty().withMessage( "Please upload attachments").isArray({min: 1 , max:97}).withMessage("Attachments must be between 1 and 5 files"),
+];
+
+const chatIdValidator = () => [
+    param("id" , "Please enter a valid chat Id").notEmpty(),
+];
+
+const renameValidator = () => [
+    body("name" , "Please enter a valid new name").notEmpty(),
+    param("id" , "Please enter a valid Id").notEmpty(),
+];
+
+
+const validateHandler = (req , res , next) => {
+    const errors = validationResult(req);
+
+    const errorMessages = errors.array().map(error => error.msg).join(", ");
+    
+    if(errors.isEmpty()){
+        return next();
+    }else{
+        console.log("Validation errors:", errorMessages);
+        next(new ErrorHandler(errorMessages , 400));
+    }
+}
+
+
+export {
+    registerValidator ,
+    validateHandler ,
+    loginValidator ,
+    newGroupValidator,
+    addMemberValidator,
+    removeMemberValidator,
+    sendAttachmentsValidator,
+    chatIdValidator,
+    renameValidator
+}
--- server/routes/chat.routes.js
+++ server/routes/chat.routes.js
@@ -2,31 +2,31 @@ import express from "express";
 import { isAuthenticated } from "../middlewares/auth.js";
 import { addMembers, deleteChat, getChatDetails, getMessages, getMyChats, getMyGroups, leaveGroup, newGroupChat, removeMembers, renameGroup, sendAttachments } from "../controllers/chat.js";
 import { attachmentsMulter } from "../middlewares/multer.js";
+import { addMemberValidator, chatIdValidator, newGroupValidator, removeMemberValidator, renameValidator, sendAttachmentsValidator, validateHandler } from "../lib/validators.js";
 
 
 const app = express.Router();
 
 app.use(isAuthenticated);
 
-app.post("/new" , newGroupChat);
+app.post("/new" , newGroupValidator() , validateHandler , newGroupChat);
 app.get("/my" , getMyChats)
 app.get("/my/groups" , getMyGroups);
 
+app.put("/addMembers" , addMemberValidator() , validateHandler , addMembers);
+app.put("/removeMembers" , removeMemberValidator() , validateHandler , removeMembers);
 
-app.put("/addMembers" , addMembers);
-app.put("/removeMembers" , removeMembers);
+app.delete("/leave/:id" , chatIdValidator() , validateHandler , leaveGroup);
 
-app.delete("/leave/:id" , leaveGroup);
+app.post("/message" , attachmentsMulter , sendAttachmentsValidator() , validateHandler , sendAttachments);
 
-app.post("/message" , attachmentsMulter , sendAttachments);
-
-app.get("/message/:id" , getMessages)
+app.get("/message/:id" , chatIdValidator() , validateHandler , getMessages)
 
 
 app.route("/:id")
-    .get(getChatDetails)
-    .put(renameGroup)
-    .delete(deleteChat)
+    .get(chatIdValidator() , validateHandler ,getChatDetails)
+    .put(renameValidator() , validateHandler ,renameGroup)
+    .delete(chatIdValidator() , validateHandler , deleteChat)
 
 
 export default app;
